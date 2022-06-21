3bcc280cc102a2f3b6dd4ef0b2873ad93abe8b2d
CeMuPaMuDa <bepyc9i@gmail.com>
2022-06-21 23:13:38 +0300

    add any configuration

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative "boot"
 
 require "rails"
-# Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -14,24 +13,23 @@ require "action_view/railtie"
 # require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module News7
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-
-    # Don't generate system test files.
     config.generators.system_tests = nil
+    config.i18n.available_locales = %i[en ru uk]
+    config.i18n.default_locale = :ru
+    config.time_zone = 'Kyiv'
+    config.generators do |g|
+      g.orm              :active_record
+      g.template_engine  :slim
+      g.system_tests     nil
+      g.test_framework   nil
+      g.helper           false
+      g.stylesheets      false
+      g.javascripts      false
+    end
   end
 end
