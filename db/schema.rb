cd7683d927e9f3e75b261367a66c4330439685f7
CeMuPaMuDa <bepyc9i@gmail.com>
2022-06-28 13:12:17 +0300

    add post link

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_23_110849) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_28_095952) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", comment: "Статьи", force: :cascade do |t|
+    t.string "title", comment: "Заголовок статьи"
+    t.text "content", comment: "Содержимое статьи"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", comment: "Пользователь(Автор)", force: :cascade do |t|
     t.string "name", comment: "Имя пользователя"
