e4e7b85125ef21bec49e6a08257d6c11db5cf251
CeMuPaMuDa <bepyc9i@gmail.com>
2022-07-05 02:10:04 +0300

    i18n dropdawn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,29 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  around_action :switch_locale
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  def switch_locale(&action)
+    locale = locale_from_url || I18n.default_locale
+    I18n.with_locale locale, &action
+  end
+
+  def locale_from_url
+    loc = params[:locale]
+
+    return loc if I18n.available_locales.map(&:to_s).include?(loc)
+
+    nil
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :posts
-  devise_for :users
+  scope '(:locale)' do
+    resources :posts
+    devise_for :users
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "home#index"
+    # Defines the root path route ("/")
+    root "home#index"
+  end
 end
