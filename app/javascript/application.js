79d0c19729f4a2e3f3a3086936990f36b5392c38
CeMuPaMuDa <bepyc9i@gmail.com>
2022-07-05 15:33:00 +0300

    add heart icon

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-
+//= require font_awesome5
 import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
