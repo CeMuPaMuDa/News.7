aaf9fb04e43197839b5ba9d702246fa206a025c2
CeMuPaMuDa <bepyc9i@gmail.com>
2022-07-05 15:36:08 +0300

    fix poper.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,8 +8,8 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
-pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.5/lib/index.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.2/lib/index.js"
 pin "popper", to: 'popper.js', preload: true
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
+pin "bootstrap", to: 'bootstrap.min.js', preload: true
 pin "fontawesome", to: "https://ga.jspm.io/npm:fontawesome@5.6.3/index.js"
 pin "fontawesome-free", to: "https://ga.jspm.io/npm:fontawesome-free@1.0.4/attribution.js"
