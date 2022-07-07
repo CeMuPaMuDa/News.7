ae6f9fe127ade782f71a133f16ef5998a331c034
CeMuPaMuDa <bepyc9i@gmail.com>
2022-07-07 15:25:15 +0300

    red color to like click

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,26 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require font_awesome5
 import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
 import "popper"
 import "bootstrap"
-import "apps/like.js"
+
+const changeColorToRed = (element) => {
+  element.classList.add('heart-red');
+}
+
+const options = {
+  "capture": false,
+  "once": true,
+  "passive": false
+}
+const heartButton = window.document.querySelector('.heart')
+
+
+const redHeart = (event) => {
+  const likeIcon = event.currentTarget
+  changeColorToRed(likeIcon)
+}
+
+heartButton.addEventListener("click", redHeart)
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,5 +29,7 @@ Rails.application.configure do
   config.active_support.disallowed_deprecation_warnings = []
   config.active_record.migration_error = :page_load
   config.active_record.verbose_query_logs = true
+  config.assets.debug = true
   config.assets.quiet = true
+  config.assets.compile = true
 end
